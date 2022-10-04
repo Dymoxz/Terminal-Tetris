696e47c2677af2a85656f3f34019b9291ff1e3f0
Dymo Walheer <210532@edu.ozhw.nl>
2022-10-04 21:04:11 +0200

    alles werk bhv kleru

--- fix.py
+++ fix.py
@@ -59,12 +59,14 @@ RESET = '\033[0m'
 
 #------------ Variables ------------#
 count = 0
-moved = False
 
 x = 0
 abx = 0
 aby = 0
 y = 0
+
+colour_list = []
+shape_list = []
 
 time_lapsed = 0
 start_time = time.time()
@@ -88,24 +90,27 @@ def print_board(board):
             if c == background:
                 print(c + ' ', end='')
             else:
-                # if spot is occupied print the shape wtih the correct color
                 print(get_color_escape(currentColour[0], currentColour[1], currentColour[2]) + c + RESET + ' ', end='')
         print()
     print()
 
 
-
+#converts time
 def time_convert(sec):
     mins = sec // 60
     sec = sec % 60
     hours = mins // 60
     mins = mins % 60
 
+#moving the shape down and deleting the old one
 def gravity(board, shape):
+    #shape = currentShape
     for coords in shape:
+        #---!!---#
         ABSshapecoords = list(board[coords[1]])
         ABSshapecoords[coords[0]] = background
         board[coords[1]] = ''.join(ABSshapecoords)
+    #move the shape down by 1 for every y coordinate in it
     for s in range(0,len(shape)):
         currentABS[s][1] += 1
 
@@ -117,30 +122,32 @@ def on_press(key):
     global currentShape  
     global rotation
     global leftBound
-    global moved
     global aby
+    #check wich key is pressed
     if key == keyboard.Key.esc:
         return False  # stop listener
     try:
         k = key.char  # single-char keys
     except:
         k = key.name  # other keys
+    
     if k in ['left', 'right', 'down', 'up']:
+        #if the key is right_arrow move x by 1 if it doesnt go out of bounds
         if k == 'right':
             if x + abx < 9 - width + 1:
                 x += 1
+        #if the key is left_arrow move x by -1 if it doesnt go out of bounds
         if k == 'left':
             if x + abx > 0:
                 x -= 1
+        #if the key is down_arrow do y + 1 if it doesnt go out of bounds
         if k == 'down':
             if y + aby < 20 - height:
                 y += 1
+        #if the key is up_arrow do rotation + 1 but if it is the 3rd make it rotate back to 0
         if k == 'up':
             pptation = copy.deepcopy(rotation)
-            if rotation < 3:
-                pptation += 1
-            else:
-                pptation = 0
+            pptation = pptation + 1 if rotation < 3 else 0
             currentSheap = fullShape[pptation]
             minX = min([item[0] for item in currentSheap]) + x
             maxX = max([item[0] for item in currentSheap]) + x
@@ -162,11 +169,14 @@ def on_press(key):
         for s in range(0,4):
                 currentABS[s][0] = currentShape[s][0] + x
                 currentABS[s][1] = currentShape[s][1] + y
+        # for every coord in the shape set it to the icon in this case ⯀
         for coord in currentABS:
             ABSshapecoords = list(board[coord[1]]) 
             ABSshapecoords[coord[0]] = shapeIcon
             board[coord[1]] = ''.join(ABSshapecoords)
         print_board(board)
+
+#clears the console
 def clear():
     if os.name == 'nt':
         _ = system('cls')
@@ -174,73 +184,112 @@ def clear():
         _ = system('clear')
     return _
 
+#get the width and height of the shape
 def getDimenstions():
     global width
     global height
     global rightBound
     global leftBound
     global topBound
+    #get most left, right, top and bottom bound of the shape
     leftBound = min([item[0] for item in currentShape])
     rightBound = max([item[0] for item in currentShape])
     topBound = min([item[1] for item in currentShape])
     bottomBound = max([item[1] for item in currentShape])
+    #set the width and height of the shape
     width = rightBound - leftBound + 1
     height = bottomBound - topBound + 1
 
 
 #------------------------------ Main Game ------------------------------#
-clear()
 
-currentShape = random.choice(shapes)
-fullShape = currentShape
-currentShape = currentShape[rotation]
-currentABS = copy.deepcopy(currentShape)
-fullABS = copy.deepcopy(fullShape)
-currentColour = colors[shapes.index(fullShape)]
-
-getDimenstions()
-
-print_board(board)
-
-x = leftBound
-aby = topBound
+randShapes = copy.copy(shapes)
+random.shuffle(randShapes)
 
 listener = keyboard.Listener(on_press=on_press)
 listener.start()  # start to listen on a separate thread
 
-while y + aby < 20 - height:
-    start = time.time()
-    period = datetime.datetime.now()
-    if period.second % 1 == 0 and (period - lastTime).total_seconds() >= 0.5:
-        gravity(board, currentABS)
-        lastTime = period
-        for coord in currentABS:
-            aaa = list(board[coord[1]]) 
-            aaa[coord[0]] = shapeIcon
-            board[coord[1]] = ''.join(aaa)
-        print_board(board)
-        y += 1
-    try:
-        #print(f'FPS: {round(count/time_lapsed, 2)}   |   Frames: {count}   |   Time: {round(time_lapsed, 3)} Sec.   |   Rotation: {rotation}', end='\r')
-        print(f'width: {width}  |   height: {height}    |   x: {x + abx}  |   y: {y + aby}', end='\r')
-        #print(f'currentShape: {currentShape}    |   currentABS: {currentABS}    |   rotation: {rotation}', end='\r')
-        #print(x + abx, y, end='\r')
-        #print(currentABS, f'x= {x}, y= {y}', end='\r')
-    except:
-        pass
-    #print(f'x: {x}   |   y: {y}', end='\r')
+#main game loop
+while True:
 
-    count += 1
-    end_time = time.time()
-    time_lapsed = end_time - start_time
-    time_convert(time_lapsed)   
-    time.sleep(max(1./60 - (time.time() - start), 0))
+    x = 0
+    abx = 0
+    aby = 0
+    y = 0
+
+    clear()
+
+    try:
+        fullShape = randShapes[0]
+        randShapes.pop(0)
+    except:
+        randShapes = copy.deepcopy(shapes)
+        random.shuffle(randShapes)
+        fullShape = randShapes[0]
+        randShapes.pop(0)
+        
+    currentShape = fullShape[rotation]
+    
+    currentABS = copy.deepcopy(currentShape)
+    fullABS = copy.deepcopy(fullShape)
+    currentColour = colors[shapes.index(fullShape)]
+    getDimenstions()
+
+    print_board(board)
+
+    x = leftBound
+    aby = topBound
+
+
+
+
+    #loop for the current shape
+    while y + aby < 20 - height:
+        start = time.time()
+        period = datetime.datetime.now()
+        if period.second % 1 == 0 and (period - lastTime).total_seconds() >= 0.5:
+            #do the gravity, boom, b-b-boom b-boom b-boom!
+            gravity(board, currentABS)
+            lastTime = period
+            for coord in currentABS:
+                aaa = list(board[coord[1]]) 
+                aaa[coord[0]] = shapeIcon
+                board[coord[1]] = ''.join(aaa)
+            print_board(board)
+            y += 1
+        try:
+            print(f'FPS: {round(count/time_lapsed, 2)}   |   Frames: {count}   |   Time: {round(time_lapsed, 3)} Sec.   |   Rotation: {rotation}', end='\r')
+            #print(f'width: {width}  |   height: {height}    |   x: {x + abx}  |   y: {y + aby}', end='\r')
+            #print(colour_list, shape_list, end='\r')
+            #print(f'currentShape: {currentShape}    |   currentABS: {currentABS}    |   rotation: {rotation}', end='\r')
+            #print(x + abx, y, end='\r')
+            #print(currentABS, f'x= {x}, y= {y}', end='\r')
+        except:
+            pass
+        count += 1
+        end_time = time.time()
+        time_lapsed = end_time - start_time
+        time_convert(time_lapsed)   
+        
+        try:
+            if colour_list[-1] != currentColour:
+                shape_list.append(currentABS)
+                colour_list.append(currentColour)
+        except:
+            shape_list.append(currentABS)
+            colour_list.append(currentColour)
+        #limit to 60 fps instead of 20.000 fps
+        time.sleep(max(1./60 - (time.time() - start), 0))
+
+
 
 
 listener.stop()
+
 clear()
 print_board(board)
+print('\n\n')
 print(f'FPS: {round(count/time_lapsed, 2)}   |   Frames: {count}   |   Time: {round(time_lapsed, 3)} Sec.   |   Rotation: {rotation}')
 print(f'X: {x + abx}   |   Y: {y + aby}')
 print(f'rotation: {rotation}')
-print()
+print('\n\n')
